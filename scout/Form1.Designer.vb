3d52158d1cce8826574ff19ca487da7b7bec6c33
unknown <horden316>
2017-10-19 14:45:25 +0800

    bugs fix

--- scout/Form1.Designer.vb
+++ scout/Form1.Designer.vb
@@ -24,31 +24,61 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(141, 47)
+        Me.Label1.Location = New System.Drawing.Point(101, 62)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(46, 15)
+        Me.Label1.Size = New System.Drawing.Size(0, 15)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(130, 177)
+        Me.Button1.Location = New System.Drawing.Point(26, 182)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Scout!"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(101, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 15)
+        Me.Label2.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(101, 108)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 15)
+        Me.Label3.TabIndex = 3
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(118, 182)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(115, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Auto Scout"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(337, 257)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
@@ -59,5 +89,8 @@ Partial Class Form1
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
 
 End Class
--- scout/Form1.vb
+++ scout/Form1.vb
@@ -1,23 +1,41 @@
 ﻿Public Class Form1
     Dim people As Integer
-    Dim cnt, i As Integer
+    Dim cnt As Integer
     Dim Type(50) As Integer
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         people = InputBox("請輸入人數", "Hi")
+        Label2.Text = "參加人數" & people & "人"
         Randomize()
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim num As Integer
-        num = Int(Rnd() * people + 1)
-        Me.Text = num.ToString
-        Select Case num
-            Case 1
-                Label1.Text = "歡迎來到空軍"
-            Case 2 To 4
-                Label1.Text = "歡迎來到海軍"
-            Case Else
-                Label1.Text = "歡迎來到陸軍"
-        End Select
+        For i = 1 To people
+            num = Int(Rnd() * people + 1)
+            If num = Type(i) Then
+                num = Int(Rnd() * people + 1)
+            End If
+            Me.Text = num.ToString
+            If Type(num) = 0 Then
+                Exit For
+            End If
+        Next
+        cnt += 1
+        If cnt <= people Then
+            Select Case num / people
+                Case 0.0 To 0.1
+                    Label1.Text = "歡迎來到空軍"
+                    Label3.Text += "空"
+                Case 0.11 To 0.4
+                    Label1.Text = "歡迎來到海軍"
+                    Label3.Text += "海"
+                Case Is > 0.4
+                    Label1.Text = "歡迎來到陸軍"
+                    Label3.Text += "陸"
+            End Select
+
+        Else
+            MessageBox.Show("抽完啦")
+        End If
     End Sub
 End Class
